19e35d2305ecc8f2e5f38c6817a4b9efcb611b9c
Ron Isaac <ronotieno@kabarak.ac.ke>
2024-10-03 11:46:05 +0300

    Version 1

---
+++ react-color-picker/src/ColorPicker.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+function ColorPicker() {
+    const [color, setColor] = useState("#FFFFFF");
+
+    const handleColorChange = (event) =>{
+        setColor(event.target.value);
+    }
+
+    return (
+        <>
+        <h1>Color Picker</h1>
+        <div className="color-picker-container" style={{backgroundColor: color}}>
+            <p>Selected color: {color}</p>
+            </div> 
+            <label>Select a color: </label>
+            <input type="color" value={color} onChange={handleColorChange} />
+        </>
+    )
+
+
+
+}
+export default ColorPicker;
